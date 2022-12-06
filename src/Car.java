b990c86a0baa85f12eed638f70eaec000d1a1121
YarionS17 <yarion1706@gmail.com>
2022-12-06 15:25:09 +0100

    Add car class

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    private String type;
+    private String engine;
+    private int seats;
+
+    //Stat points
+    private int health;
+    private int durability;
+    private int speed;
+    private int acceleration;
+
+    public Car(String naam, String engine, int seats, int health, int durability, int speed, int acceleration){
+
+    }
+}
