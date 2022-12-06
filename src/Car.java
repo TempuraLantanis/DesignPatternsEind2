4918a5225638670cf627033429c8e6a7551833b6
YarionS17 <yarion1706@gmail.com>
2022-12-06 15:42:37 +0100

    getters setters

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,5 @@
 public class Car {
     private String type;
-    private String engine;
     private int seats;
 
     //Stat points
@@ -9,7 +8,41 @@ public class Car {
     private int speed;
     private int acceleration;
 
-    public Car(String naam, String engine, int seats, int health, int durability, int speed, int acceleration){
+    public Car(
+        //String naam, String engine, int seats, int health, int durability, int speed, int acceleration
+        ){
 
+    }
+
+    public void setHealth(int newHealth){
+        this.health = newHealth;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void setDurability(int newDurability){
+        this.durability = newDurability;
+    }
+
+    public int getDurability(){
+        return this.durability;
+    }
+
+    public void setSpeed(int newSpeed){
+        this.speed = newSpeed;
+    }
+
+    public int setSpeed(){
+        return this.speed;
+    }
+
+    public void setAcceleration(int newAcceleration){
+        this.acceleration = newAcceleration;
+    }
+
+    public int getAcceleration(){
+        return this.acceleration;
     }
 }
