2c7911d9d0ee6efee129273b45bd553286bb0b72
Tempura <33734673+TempuraLantanis@users.noreply.github.com>
2023-01-05 17:59:07 +0100

    Revert "complete decorator and changes to builder"

--- src/SportsCarBuilder.java
+++ src/SportsCarBuilder.java
@@ -3,7 +3,6 @@ public class SportsCarBuilder implements CarBuilder {
   
     public SportsCarBuilder() {
       car = new Car();
-      car.setGps(true); // Only Sports cars have gps
     }
   
     
